f7de30913c79c40b50293cdf19c4503498d82de2
cristibucni <cristian.bucnaru@gmail.com>
2020-09-10 20:45:13 +0300

    Final

--- client/src/components/dashboard/CreateProfile.js
+++ client/src/components/dashboard/CreateProfile.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import _ from 'lodash';
 import axios from 'axios';
+import TextField from '@material-ui/core/TextField';
 class CreateProfile extends Component {
     constructor(props) {
         super(props);
@@ -93,32 +94,33 @@ class CreateProfile extends Component {
                 {' '}
                 <div className="question-container">
                     <li className="question-title">Descriere personală</li>
-                    <input type="text" onChange={(e) => this.handleOnChange(e)} name="bio" placeholder="" className="question-user-input-answer" />
+                    <TextField fullWidth id="standard-basic" label="Bio" onChange={(e) => this.handleOnChange(e)} name="bio" placeholder="" />
                 </div>
                 <div className="question-container">
                     <li className="question-title">Site personal</li>
-                    <input type="text" onChange={(e) => this.handleOnChange(e)} name="website" placeholder="" className="question-user-input-answer" />
+                    <TextField id="standard-basic" label="Website" onChange={(e) => this.handleOnChange(e)} name="website" placeholder="" />
                 </div>
                 <div className="question-container">
                     <li className="question-title">Locație</li>
-                    <input type="text" onChange={(e) => this.handleOnChange(e)} name="location" placeholder="" className="question-user-input-answer" />
+                    <TextField id="standard-basic" label="Locatie" onChange={(e) => this.handleOnChange(e)} name="location" placeholder="" />
                 </div>
                 <div className="question-container">
                     <li className="question-title">Universitate</li>
-                    <input type="text" onChange={(e) => this.handleOnChange(e)} name="university" placeholder="" className="question-user-input-answer" />
+                    <TextField id="standard-basic" label="Universitate" onChange={(e) => this.handleOnChange(e)} name="university" placeholder="" />
                 </div>
                 <div className="question-container">
                     <li className="question-title">Facultate</li>
-                    <input type="text" onChange={(e) => this.handleOnChange(e)} name="faculty" placeholder="" className="question-user-input-answer" />
+                    <TextField id="standard-basic" label="Facultate" onChange={(e) => this.handleOnChange(e)} name="faculty" placeholder="" />
                 </div>
                 <div className="question-container">
                     <li className="question-title">Aptitudini</li>
-                    <input type="text" onChange={(e) => this.handleOnSkillChange(e)} name="skills" placeholder="" className="question-user-input-answer" />
+                    <TextField id="standard-basic" label="Aptitudini" onChange={(e) => this.handleOnChange(e)} name="skills" placeholder="" />
                 </div>
                 <div className="row">
                     <div className="col">
                         <div className="question-container">
                             <li className="question-title">Facebook</li>
+                            <TextField id="standard-basic" label="Aptitudini" onChange={(e) => this.handleOnChange(e)} name="skills" placeholder="" />
                             <input type="text" onChange={(e) => this.handleOnChange(e)} name="skills" placeholder="" className="question-user-input-answer" />
                         </div>
                     </div>
